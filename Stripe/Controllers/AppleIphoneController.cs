49fbc7d5ce3b52f261699032fd9d0f8386391a61
Ruchika <Ruchika@DESKTOP-VAC6K1T>
2017-01-10 22:28:37 +0530

    Add project files.

---
+++ Stripe/Controllers/AppleIphoneController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using Stripe.Models;
+
+namespace Stripe.Controllers
+{
+    public class AppleIphoneController : Controller
+    {
+        // GET: AppleIphone
+        public ActionResult Index()
+        {
+            string stripePublishableKey = ConfigurationManager.AppSettings["stripePublishableKey"];
+            var model = new AppleIphoneViewModels() { StripePublishableKey = stripePublishableKey };
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Charge(ChargeViewModel chargeViewModel) {
+
+            var chargeOption = new StripeChargeCreateOptions()
+            {
+                //required
+                Amount = 60000,
+                Currency = "usd",
+                SourceTokenOrExistingSourceId = chargeViewModel.StripeToken,
+
+                //optional
+                ReceiptEmail = chargeViewModel.StripeEmail
+               
+            };
+
+            var stripeChargeService = new StripeChargeService();
+            var stripeService = stripeChargeService.Create(chargeOption);
+            return RedirectToAction("Confirmation");
+
+        }
+
+        public ActionResult Confirmation() {
+
+            return View();
+        }
+    }
+}
---
+++ Stripe/Models/AppleIphoneViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stripe.Models
+{
+    public class AppleIphoneViewModels
+    {
+        public string StripePublishableKey { get; set; }
+    }
+
+    public class ChargeViewModel {
+
+        public string StripeToken { get; set; }
+        public string StripeEmail { get; set; }
+    }
+}
